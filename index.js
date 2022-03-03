34a4cbdc152faadf89d332b4a2202c0dbf1306a5
Stefan <stefan.andelic@ibm.com>
2022-03-02 21:16:08 -0500

    changes

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const staging = require("./config/staging_env.json");
+const express = require('express')
+const staging = require('./config/staging_env.json')
+const dotenv = require('dotenv')
+const team = require('./routes/team_api_endpoint')
+const project = require('./routes/project_api_endpoint')
+const auth = require('./routes/auth_api_endpoint')
 
-const team = require("./routes/team_api_endpoint");
-const project = require("./routes/project_api_endpoint");
-const auth = require("./routes/auth_api_endpoint");
-
-const app = express();
-app.use(express.json());
-
+const app = express()
+app.use(express.json())
+dotenv.config()
 //routes
-app.use("/ibm/api/v1/team", team);
-app.use("/ibm/api/v1/project", project);
-app.use("/ibm/api/v1/auth", auth);
+app.use('/ibm/api/v1/team', team)
+app.use('/ibm/api/v1/project', project)
+app.use('/ibm/api/v1/auth', auth)
 
 //connects to the database
-require("./connection/db_connection")();
+require('./connection/db_connection')()
 
 //main
 function start() {
-  console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`)
 }
-const port = process.env.PORT || staging.port;
-const server = app.listen(port, start);
+const port = process.env.PORT || staging.port
+const server = app.listen(port, start)
 
-module.exports = server;
+module.exports = server
